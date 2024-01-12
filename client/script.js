8b7547f5afd29f0658406994f64cbc051a82df1e
Mostafa Zewail77 <mostafazewail77@gmail.com>
2024-01-12 19:45:17 +0200

    Update server url

--- client/script.js
+++ client/script.js
@@ -96,7 +96,7 @@ const handleSubmit = async (e)=>{
     } 
 
     // Fetch data from server => bot's response
-    const response = await fetch('http://localhost:5000',{
+    const response = await fetch('https://botify-qupb.onrender.com/',{
       method:'POST',
       headers:{
         'Content-Type':'application/json',
